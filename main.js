7b8d52b919994e1982a529a8f95c198a4558545b
15veronica <106267538+15veronica@users.noreply.github.com>
2024-11-19 12:05:06 -0300

    Update main.js

--- main.js
+++ main.js
@@ -15,9 +15,9 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2024-12-05T00:00:00");
-const tempoObjetivo2 = new Date("2024-12-05T00:00:00");
-const tempoObjetivo3 = new Date("2024-12-T00:00:00");
+const tempoObjetivo1 = new Date("2025-01-12T00:00:00");
+const tempoObjetivo2 = new Date("2024-12-27T00:00:00");
+const tempoObjetivo3 = new Date("2025-03-07T00:00:00");
 const tempoObjetivo4 = new Date("2025-01-01T00:00:00");
 
 const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
